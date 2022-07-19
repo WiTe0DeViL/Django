ca1cd84dca6650583d74f02dc7adfa4842711951
WiTe0DeViL <surentamarai222@gmail.com>
2022-07-19 10:15:00 +0530

    Revert "gender to suren"
    This reverts commit c7560e6b025ba5c587e44df3f94a23b712ee6616.

--- store/models.py
+++ store/models.py
@@ -43,7 +43,14 @@ class Customer(models.Model):
     ]
 
     first_name = models.CharField(max_length=20)
+
     last_name = models.CharField(max_length=20)
+<<<<<<< HEAD
+    Email = models.EmailField(unique=True)
+=======
+    sunni = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=25)
     birth_date = models.DateField(null=True)
